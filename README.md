cf1fb437f9f492ef1b13da67e23a89fe92a58311
milosmikovic <losmicarina@gmail.com>
2019-08-19 17:45:40 +0200

    komit1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RG1xy-simulacija-automobila-koji-izbegava-prepreke
+
+
+                    SIMULACIJA AUTOMOBILA KOJI IZBEGAVA PREPREKE :D
---
+++ rep1/main.c
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+#include <math.h>
+#include <GL/glut.h>
+
+/* Fleg koji odredjuje stanje tajmera. */
+static int timer_active;
+
+/* Deklaracije callback funkcija. */
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_timer(int value);
+static void on_reshape(int width, int height);
+static void on_display(void);
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    glutKeyboardFunc(on_keyboard);
+    glutReshapeFunc(on_reshape);
+    glutDisplayFunc(on_display);
+
+    timer_active = 0;
+
+    glClearColor(1, 1, 1, 0);
+    glEnable(GL_DEPTH_TEST);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+static void on_keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+    case 27:
+        exit(0);break;
+    }
+}
+
+static void on_timer(int value)
+{
+    /* Proverava se da li callback dolazi od odgovarajuceg tajmera. */
+    if (value != 0)
+        return;
+
+    /* Forsira se ponovno iscrtavanje prozora. */
+    glutPostRedisplay();
+
+    /* Po potrebi se ponovo postavlja tajmer. */
+    if (timer_active)
+        glutTimerFunc(50, on_timer, 0);
+}
+
+static void on_reshape(int width, int height)
+{
+    /* Postavlja se viewport. */
+    glViewport(0, 0, width, height);
+
+    /* Postavljaju se parametri projekcije. */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, (float) width / height, 1, 1500);
+}
+
+static void on_display(void)
+{
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* Postavlja se vidna tacka. */
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2,2,-4,
+                2,0,2,
+                0.0,1.0,0.0);
+
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(100,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(-100,0,0);
+        
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,100,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,-100,0);
+
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,100);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,-100);
+    glEnd();
+
+    /* Postavlja se nova slika u prozor. */
+    glutSwapBuffers();
+}
